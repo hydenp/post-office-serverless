1afbce538e7ee4b548795070196340d7c91edca0
Hyden Polikoff <hyden.dev@gmail.com>
2022-08-19 16:39:03 -0600

    remove duplicate aws_ecr_repository resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,11 +31,6 @@ resource "aws_ecr_repository" "lambda_ecr_repo" {
   name = local.ecr_repository_name
 }
 
-data "aws_ecr_image" "lambda_image" {
-  repository_name = local.ecr_repository_name
-  image_tag       = local.ecr_image_tag
-}
-
 
 ##################################################
 # Lambda Function Provisioning
